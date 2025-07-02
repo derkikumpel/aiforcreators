bbac035eb2af3e1416ca37ae8d59757546bfce89
derkikumpel <derkikumpel@gmail.com>
2025-07-02 10:19:09 +0200

    Update screenshot-tools.mjs

--- scripts/screenshot-tools.mjs
+++ scripts/screenshot-tools.mjs
@@ -5,8 +5,8 @@ import { v2 as cloudinary } from 'cloudinary';
 // Cloudinary-Konfiguration
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 async function captureScreenshot(tool) {
